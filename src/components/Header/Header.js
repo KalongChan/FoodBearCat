9552366acceb87405a036151b715260e97bbe05a
Kalong <cklong0133@gmail.com>
2023-02-24 02:54:25 +0800

    Added collapsing header && main header new style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import classes from "./Header.module.css";
 import {HiOutlineShoppingCart} from "react-icons/hi";
 import Link from "next/link";
-import {useEffect, useState} from "react";
+import {Fragment, useEffect, useState} from "react";
 import {useSelector} from "react-redux";
 import {signIn, signOut, useSession} from "next-auth/react";
 
 const Header = () => {
   const items = useSelector((state) => state.cart.items);
   const [btnAnimation, setBtnAnimation] = useState(false);
+  const [collaps, setCollaps] = useState(false);
   const totalCartItems = items.reduce(
     (total, item) => total + item.quantity,
     0
@@ -19,33 +20,101 @@ const Header = () => {
     }, 300);
   }, [totalCartItems]);
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", () => {
+        if (window.pageYOffset > 80) {
+          setCollaps(true);
+        } else {
+          setCollaps(false);
+        }
+      });
+    }
+  }, []);
+
   const {data: session, status} = useSession();
 
   return (
-    <header className={classes.header}>
-      <div className={classes.container}>
-        <div className={classes.logo}>
-          <Link href="/">FoodBearCat</Link>
+    <Fragment>
+      <div className={classes["main-header"]}>
+        {/* Header left */}
+        <div className={classes["header-left"]}>
+          <div className={classes.logo}>
+            <Link href="/">FoodBearCat</Link>
+          </div>
         </div>
-        <div className={classes.headerLinks}>
-          {session && session.admin && <Link href="/admin">WebPanel</Link>}
-          <Link href="/">Home</Link>
-
-          <Link href="/orders">Orders</Link>
-          <span>About</span>
-          {!session && <Link href="/signin">Login</Link>}
-          {session && <span onClick={signOut}>Logout</span>}
-          {/* <span onClick={signIn}>SignIN</span> */}
-          <Link
-            href="/cart"
-            className={`${classes.cart} ${btnAnimation ? classes.bump : ""}`}
-          >
-            <HiOutlineShoppingCart />
-            <span className={classes["cart-number"]}>{totalCartItems}</span>
-          </Link>
+        {/* Header Right */}
+        <div className={classes.nav}>
+          <ul>
+            <li>
+              {session && session.admin && <Link href="/admin">WebPanel</Link>}
+            </li>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/orders">Orders</Link>
+            </li>
+            <li>About</li>
+            <li>
+              <Link href="/orders">Orders</Link>
+            </li>
+            <li>
+              {!session && <Link href="/signin">Login</Link>}
+              {session && <span onClick={signOut}>Logout</span>}
+            </li>
+            <li>
+              <Link
+                href="/cart"
+                className={`${classes.cart} ${
+                  btnAnimation ? classes.bump : ""
+                }`}
+              >
+                <HiOutlineShoppingCart />
+                <span className={classes["cart-number"]}>{totalCartItems}</span>
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
-    </header>
+
+      <div
+        className={`${classes["collapsing-header"]} ${
+          collaps ? classes["show-collaps-down"] : ""
+        } ${!collaps ? classes["show-collaps-up"] : ""}`}
+      >
+        <ul>
+          <li>
+            {session && session.admin && <Link href="/admin">WebPanel</Link>}
+          </li>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/orders">Orders</Link>
+          </li>
+          <li>About</li>
+          <li>
+            <Link href="/orders">Orders</Link>
+          </li>
+          <li>
+            {!session && <Link href="/signin">Login</Link>}
+            {session && <span onClick={signOut}>Logout</span>}
+          </li>
+          <li>
+            <Link
+              href="/cart"
+              className={`${classes.cart} ${btnAnimation ? classes.bump : ""}`}
+            >
+              <HiOutlineShoppingCart />
+              <span className={classes["small-cart-number"]}>
+                {totalCartItems}
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </Fragment>
   );
 };
 export default Header;
