57ef9b452643034bfc435cc10294e68256fa28f3
Kalong <cklong0133@gmail.com>
2023-02-27 23:52:53 +0800

    Added about page

---
+++ src/components/About/About.js
@@ -0,0 +1,33 @@
+import CartContainer from "@/UI/CartContainer/CartContainer";
+import Container from "@/UI/Container/Container";
+import Image from "next/image";
+import classes from "./About.module.css";
+
+const About = () => {
+  return (
+    <Container>
+      <div className={classes["about-title"]}>
+        <h2>About</h2>
+      </div>
+      <div className={classes["about-info"]}>
+        <div className={classes["image"]}>
+          <Image
+            src="/img/computer_programming_man.png"
+            alt=""
+            width={280}
+            height={254}
+          ></Image>
+        </div>
+        <div className={classes["about-text"]}>
+          This is a food ordering website build with Next.js, Redux, and MongoDB
+          by{" "}
+          <a href="https://github.com/KalongChan" target="_blank">
+            Kalong Chan
+          </a>{" "}
+          to enhance his coding skills.
+        </div>
+      </div>
+    </Container>
+  );
+};
+export default About;
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import About from "@/components/About/About";
+import {Fade} from "react-awesome-reveal";
+
+function about() {
+  return (
+    <Fade>
+      <About />
+    </Fade>
+  );
+}
+
+export default about;
