dc4655c0be4f6ed77ff6d2bd8b8ed8d68da1f644
KalongChan <82305211+KalongChan@users.noreply.github.com>
2023-03-02 18:43:13 +0800

    Added Register form register error message

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -38,9 +38,7 @@ const Register = (props) => {
               <div className={classes.title}>Register</div>
               <div className={classes["form-item"]}>
                 <div className={classes["error-message"]}>
-                  {errors.username && touched.username
-                    ? errors.username
-                    : props.error}
+                  {props.registerError ? props.registerError : ""}
                 </div>
 
                 <div className={classes["form-row"]}>
